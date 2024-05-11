37609a77c31ef57af9d3c86167e37f86ccdd64f8
serdeshnow <vserdes1977@mail.ru>
2024-05-11 14:19:45 +0300

    upd: Create header

---
+++ app/src/widgets/Header/Header.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import logo from "../../assets/svg/logo.svg";
+import Button from "../Button/Button.jsx";
+
+export default function Header({ transparent = false }) {
+  return (
+    <header className="header">
+      <div className="img_wrapper">
+        <a href="/">
+          <img src={logo} alt="" />
+        </a>
+      </div>
+      <div className="panel_wrapper">
+        <nav className="nav_wrapper">
+          <ul className="nav_list">
+            <li className="list_item">
+              <a href="/">Контакты</a>
+            </li>
+            <li className="list_item">
+              <a href="/">Контакты</a>
+            </li>
+            <li className="list_item">
+              <a href="/">Контакты</a>
+            </li>
+          </ul>
+        </nav>
+        <Button disabled="true">Войти</Button>
+      </div>
+    </header>
+  );
+}
