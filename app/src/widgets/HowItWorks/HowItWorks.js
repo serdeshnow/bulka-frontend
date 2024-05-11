5a78a293abcddd30d4ea2ab2351d601fe15d1226
serdeshnow <vserdes1977@mail.ru>
2024-05-11 18:38:40 +0300

    upd: delete /Pages & upd: HowItWorks

--- app/src/widgets/HowItWorks/HowItWorks.js
+++ app/src/widgets/HowItWorks/HowItWorks.js
@@ -1,28 +1,27 @@
 import React from "react";
-import './HowItWorks.css'
-import img1 from '../../assets/img/HowItWorks/img1.svg'
-import img2 from '../../assets/img/HowItWorks/img2.svg'
-import img3 from '../../assets/img/HowItWorks/img3.svg'
-
+import "./HowItWorks.css";
+import img1 from "../../assets/img/HowItWorks/img1.svg";
+import img2 from "../../assets/img/HowItWorks/img2.svg";
+import img3 from "../../assets/img/HowItWorks/img3.svg";
 
 export default function HowItWorks() {
-    return (
-        <div className="howItWorks">
-            <p className="work_title">как это работает</p>
-            <div className="work_wrapper">
-                <div className="work_elem">
-                    <img src={img1} className="work_img1" />
-                    <p>соберите корзину</p>
-                </div>
-                <div className="work_elem">
-                    <img src={img2} className="work_img2" />
-                    <p>укажите желаемое время доставки</p>
-                </div>
-                <div className="work_elem">
-                    <img src={img3} className="work_img3" />
-                    <p>наслаждайтесь свежей выпечкой</p>
-                </div>
-            </div>
+  return (
+    <div className="howItWorks">
+      <p className="work_title">как это работает</p>
+      <div className="work_wrapper">
+        <div className="work_elem">
+          <img src={img1} className="work_img1" alt="" />
+          <p>соберите корзину</p>
         </div>
-    );
+        <div className="work_elem">
+          <img src={img2} className="work_img2" alt="" />
+          <p>укажите желаемое время доставки</p>
+        </div>
+        <div className="work_elem">
+          <img src={img3} className="work_img3" alt="" />
+          <p>наслаждайтесь свежей выпечкой</p>
+        </div>
+      </div>
+    </div>
+  );
 }
