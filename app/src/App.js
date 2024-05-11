812f361532ddabdf49a735b31f56f61fd8dc0b5f
serdeshnow <vserdes1977@mail.ru>
2024-05-11 18:44:37 +0300

    upd: comment Test

--- app/src/App.js
+++ app/src/App.js
@@ -1,19 +1,14 @@
 import "./styles/App.css";
 import Header from "./widgets/Header/Header.jsx";
-import Test from "./widgets/Test/Test.jsx";
+// import Test from "./widgets/Test/Test.jsx";
 import Slider from "./widgets/Slider/Slider.js";
 import HowItWorks from "./widgets/HowItWorks/HowItWorks.js";
-
-// import scroller1 from "./assets/img/scroller/1.png";
-// import scroller2 from "./assets/img/scroller/2.png";
-// import scroller3 from "./assets/img/scroller/3.png";
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <p>hui</p>
-      <Test></Test>
+      {/* <Test></Test> */}
       <Slider />
       <HowItWorks />
     </div>
