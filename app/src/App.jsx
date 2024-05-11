24f4a30b78dc7565dec17cb6f5132e1637c0de79
serdeshnow <vserdes1977@mail.ru>
2024-05-11 19:00:24 +0300

    upd: App.jsx 4 routing

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -1,11 +1,17 @@
 import "./styles/App.css";
 import Header from "./widgets/Header/Header.jsx";
-import Slider from "./widgets/Slider/Slider.js";
-import HowItWorks from "./widgets/HowItWorks/HowItWorks.js";
+import Slider from "./widgets/Slider/Slider.jsx";
+import HowItWorks from "./widgets/HowItWorks/HowItWorks.jsx";
 
 function App() {
   return (
     <div className="App">
+      <header>
+        <a href="/">Home</a>
+        <a href="/">About</a>
+        <a href="/">Catalog</a>
+      </header>
+      <h1>some texxt</h1>
       <Header />
       <Slider />
       <HowItWorks />
