8216416d834e9bbb3e2ef3d7c802c5af0ec814ec
serdeshnow <vserdes1977@mail.ru>
2024-05-12 16:02:52 +0300

    upd: pages

---
+++ app/src/pages/CartPage.jsx
@@ -0,0 +1,7 @@
+export const CartPage = () => {
+  return (
+    <div className="page_wrapper">
+      <h1>Shoopping cart page</h1>
+    </div>
+  );
+};
---
+++ app/src/pages/ChatPage.jsx
@@ -0,0 +1,7 @@
+export const ChatPage = () => {
+  return (
+    <div className="page_wrapper">
+      <h1>Chat</h1>
+    </div>
+  );
+};
