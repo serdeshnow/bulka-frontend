b78d80cbd395b8c05b5991bf0b361bf42ad23777
serdeshnow <vserdes1977@mail.ru>
2024-05-22 20:19:46 +0300

    upd: SliderContainer

--- app/src/widgets/Containers/SliderContainer/SliderContainer.jsx
+++ app/src/widgets/Containers/SliderContainer/SliderContainer.jsx
@@ -1,30 +1,42 @@
-// import React, { useEffect } from "react";
-import React from "react";
+import React, { useEffect } from "react";
+// import React from "react";
 import "./SliderContainer.css";
 import img1 from "../../../assets/img/Slider/1.png";
-// import axios from "axios";
+import axios from "axios";
 // import img2 from "../../assets/img/Slider/2.png";
 // import img3 from "../../assets/img/Slider/3.png";
 
 export default function SliderContainer() {
   //Backend + Frontend be like
 
-  // const getting = async () => {
-  //   await axios
-  //     .get("http://95.84.137.217:3001/get-stats")
-  //     .then((res) => {
-  //       console.log(res);
-  //       console.log(res.data);
-  //       console.log(res.data.cpu); // resolve
-  //     })
-  //     .catch((e) => {
-  //       console.error(e);
-  //     }); // promise
-  // };
+  const posting = async (password, phone) => {
+    let headers = {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+    };
+    let data = {
+      password: password,
+      phone: phone,
+    };
+    await axios
+      .post(`http://81.31.247.55:8080/admin/login`, data, { headers })
+      .then((res) => {
+        if (res.status === 200) {
+          let resData = res.data;
+          console.log(resData.id);
+        }
+        console.log(res.status);
+        // console.log(res.data);
+        // console.log(res.data.cpu); // resolve
+      })
+      .catch((e) => {
+        console.error(e);
+      }); // promise
+  };
 
-  // useEffect(() => {
-  //   getting();
-  // }, []);
+  useEffect(() => {
+    posting("string", 9998085267);
+  }, []);
 
   return (
     <div className="slider">
